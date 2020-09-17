bc3fcfc325b4eb28c02e4ad1772c24503e79935f
Brandon Gamble <typhoongraphics@yahoo.com>
2020-09-16 22:37:24 -0400

    g

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ function About() {
                 <p>This is a TodoList app v1.1.0. </p>
                 <p>State persists in local storage so state can persist on a single device.</p>
                 <p>Added drag and drop reordering (click/tap and hold for 300ms).</p>
-                <p>Items are now editable.</p>
+                <p>Items are now editable/</p>
             </div>
         </React.Fragment>
     )
